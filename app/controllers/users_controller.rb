1c01727e0193e76f605e07cc49264f6fb3c851ca
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-04-10 03:12:23 +0000

    [Add]userのeditページ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,5 +15,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params_update)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def user_params_update
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
