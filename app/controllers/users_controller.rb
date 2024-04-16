1317391ddd707dc342127f11959c53ac83477e8a
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-04-16 05:37:44 +0000

    [Add]他のユーザー情報を編集できないようにする終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -50,4 +52,12 @@ class UsersController < ApplicationController
   def user_params_update
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
