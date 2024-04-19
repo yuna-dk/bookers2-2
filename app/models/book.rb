a070149eb94aeec556641dfbeb751d8b2d475370
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-04-19 07:48:31 +0000

    [Add]いいね機能、モデルとコントローラー作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :favorite, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+  
+  def favorite_by?(user)
+    favorite.exists?(user_id: user.id)
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
-  
+  has_many :favorite, dependent: :destroy
+
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
@@ -17,5 +18,5 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
