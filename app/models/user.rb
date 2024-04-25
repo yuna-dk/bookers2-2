dccdb46f4b46f3684d6e9a3cb0cfb614a9c5691d
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-04-25 05:09:18 +0000

    [Add]関連付けとルーティング記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,15 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  # フォローしている関連付け
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローされている関連付け
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォローしているユーザーを取得
+  has_many :followings, through: :active_relationships, source: :followed
+  # フォロワーを取得
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
@@ -18,6 +26,21 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  # 指定したユーザーをフォローする
+  def follow(user)
+    active_relationships.create(followed_id: user.id)
+  end
+  
+  # 指定したユーザーのフォローを解除する
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  # 指定したユーザーをフォローしているかどうかを判定
+  def following?(user)
+    followings.include?(user)
   end
 
 end
