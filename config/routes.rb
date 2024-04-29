c4c6f8c53cd53af75541a2f34bfd02438907a8a1
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-04-29 02:17:19 +0000

    [Add]検索機能、ルーティング・コントローラー・ビュー作成＆記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
+  get "search" => "searches#search"
+  
   resources :books do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  
   resources :users do
     resource :relationships, only: [:create, :destroy]
   	get "followings" => "relationships#followings", as: "followings"
